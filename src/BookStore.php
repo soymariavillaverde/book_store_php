610429216a09117d9c590573f9e04c4d7a649e24
V <V@MacBook-Pro-de-V.local>
2022-01-20 15:32:25 +0100

    passed three test, and solving the next one

--- src/BookStore.php
+++ src/BookStore.php
@@ -7,34 +7,18 @@ class BookStore {
     private $basketPrice = 0;
 
     public function total(array $books): float
+    
     {
-        //sabemos si los libros son diferentes
-        $compareBooks= $this->compareBooks($books);
+        $test = array_diff([1], [1]);// o array push e ir haciendo arrays bidimensionales 
+        var_dump(count($test));
         //comprobar cantidades de libro en el array
         $quantityOfBooks = count($books);
         //calcular el precio por unidad
-        if($compareBooks == true){
-            $this->basketPrice = $quantityOfBooks*8.0*0.05;
-            return $this->basketPrice;
-        }
         $this->basketPrice = $quantityOfBooks * 8.0;
         //devolver el precio
         return $this->basketPrice;  
     }
 
- /*    //comparar libros del array para saber si son iguales
-    public function compareBooks(array $books){
-        //recorrer el array para comprobar los valores de los libros
-        $differentBooks = false;
-        for ($i=0; $i <= count($books) ; $i++) { 
-            //comparar el valor que está en el índice 0 con el siguiente
-            //if($books[$i+1] == null) break;
-
-            if ($books[$i] != $books[$i+1]){
-                $differentBooks = true;
-            }
-        }
-        return $differentBooks;
-    } */
+    
 
 }
